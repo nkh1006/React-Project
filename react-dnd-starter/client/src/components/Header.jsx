0c12e61a1f43e53505092967ff2e1e34a3e3f85d
NANGGI <mqsora@naver.com>
2021-07-21 14:06:14 +0900

    Add files via upload

---
+++ react-dnd-starter/client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className={"row"}>
+            <p className={"page-header"}>
+                Trello Dashboard 👓
+            </p>
+        </div>
+    )
+}
+
+export default Header;
